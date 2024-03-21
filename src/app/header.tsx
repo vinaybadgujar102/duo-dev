4046f5a153225e7026ace7ba143caf17b41d1aba
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-03-21 18:29:59 +0530

    comple nav header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,28 +1,77 @@
 "use client";
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export function Header() {
+import { signIn, signOut, useSession } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogInIcon, LogOutIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+function AccountDropdown() {
   const session = useSession();
+  const isLoggedIn = !!session.data;
 
   return (
-    <header>
-      <div>
-        {session.data ? (
-          <Button onClick={() => signOut()}>Sign Out</Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"link"}>
+          <Avatar className="mr-2">
+            <AvatarImage src={session.data?.user?.image ?? ""} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+
+          {session.data?.user?.name}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuSeparator />
+        {isLoggedIn ? (
+          <DropdownMenuItem onClick={() => signOut()}>
+            <LogOutIcon className="mr-2" />
+            Sign Out
+          </DropdownMenuItem>
         ) : (
-          <Button
+          <DropdownMenuItem
             onClick={(e) => {
               e.preventDefault();
               signIn("google");
             }}
           >
+            <LogInIcon className="mr-2" />
             Sign In
-          </Button>
+          </DropdownMenuItem>
         )}
-        {session.data?.user?.name}
-        <ModeToggle />
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export function Header() {
+  return (
+    <header className="bg-gray-100 py-2 dark:bg-gray-900 container mx-auto">
+      <div className="flex justify-between items-center">
+        <Link
+          href="/"
+          className="flex gap-2 items-center text-xl hover:underlined"
+        >
+          DueDev
+        </Link>
+        <div className="flex items-center gap-4">
+          <AccountDropdown />
+
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
